9dee7a09f300b0ebf2c2bcd346db2efbee323600
Ali Masarwe <alis.masarwe@gmail.com>
2020-02-29 05:10:55 -0800

    Defined addParsedAgrs in every creation command

--- src/controller/commands/dna_creation_commands.h
+++ src/controller/commands/dna_creation_commands.h
@@ -6,6 +6,7 @@
 #define DNANALYZER_DNA_CREATION_COMMANDS_H
 
 #include <vector>
+#include <cstdlib>
 #include "../command.h"
 
 class DNACreationCommands : public Command
@@ -31,11 +32,16 @@ public:
     virtual Command& putArgs(std::vector<std::string>& args);
 
 private:
-
-    void addParsedAgrs(std::string dnaSeq, std::string m_name);
+    inline void addParsedAgrs(std::string dnaSeq, std::string m_name);
     std::string m_dnaSeq;
     std::string m_name;
 };
+
+inline void NewCommand::addParsedAgrs(std::string dnaSeq, std::string name)
+{
+    m_dnaSeq = dnaSeq;
+    m_name = name;
+}
 
 class LoadCommand : public DNACreationCommands {
 public:
@@ -44,11 +50,16 @@ public:
     virtual Command& putArgs(std::vector<std::string>& args);
 
 private:
-
-    void addParsedAgrs(std::string fileName, std::string m_name);
+    inline void addParsedAgrs(std::string fileName, std::string m_name);
     std::string m_fileName;
     std::string m_name;
 };
+
+inline void LoadCommand::addParsedAgrs(std::string fileName, std::string name)
+{
+    m_fileName = fileName;
+    m_name = name;
+}
 
 class DupCommand : public DNACreationCommands {
 public:
@@ -57,12 +68,16 @@ public:
     virtual Command& putArgs(std::vector<std::string>& args);
 
 private:
-
-    void addParsedAgrs(std::string sequenceId, std::string m_name);
+    inline void addParsedAgrs(std::string sequenceId, std::string name);
     size_t m_sequenceId;
     std::string m_name;
 };
 
+inline void DupCommand::addParsedAgrs(std::string sequenceId, std::string name)
+{
+    m_sequenceId = std::atoi(sequenceId.c_str());
+    m_name = name;
+}
 
 
 #endif //DNANALYZER_DNA_CREATION_COMMANDS_H
